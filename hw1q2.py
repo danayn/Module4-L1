de61acb155b0b7e4b62165dd3b4f6774ce8988e1
Daniel Kassaye <danielkassaye@daniels-air-4.mynetworksettings.com>
2024-07-27 14:21:58 -0700

    first commit

---
+++ hw1q2.py
@@ -0,0 +1,33 @@
+#Assignments: Python Modules
+'''
+2. Mastering Python Modules and Aliases
+
+Objective: The aim of this assignment is to enhance your proficiency in using Python modules, 
+both standard and custom, with a specific focus on importing with aliases.
+
+Task 1: Custom Module with Aliases 
+
+Problem Statement: Create a custom module named `text_utils.py` with functions for string manipulation 
+(e.g., reversing a string, capitalizing). In your main script, import this module using an alias and utilize its functions.
+
+Code Example:
+
+    # text_utils.py
+    def reverse_string(s):
+        # function to reverse a string
+
+    def capitalize_string(s):
+        # function to capitalize a string
+
+    # main.py
+    # Import text_utils using an alias and utilize its functions
+- Expected Outcome: Your main script should be able to use the functions from `text_utils.py` via an alias,
+ demonstrating an understanding of custom module creation and aliasing.
+NOTE: Ensure that all code in your file is ready to run. This means that if someone opens your file and 
+clicks the run button at the top, all code executes as intended. For example, if there are if statements,
+print statements, or for loops, they should function correctly and display output in the console as expected. 
+If you have a function, make sure the function is called and runs.
+
+The goal of this note is to ensure that all code in your Python file runs smoothly and that is has been tested.
+
+'''
---
+++ notes.py
@@ -0,0 +1,21 @@
+'''
+This is a note for Module 4 of Python Intermediate. 
+This is a Multi-line String comment. 
+
+Lesson 1 : Overview of Modules in Python
+
+https://www.w3schools.com/python/python_modules.asp
+
+
+'''
+
+# A Python Module is a file containing a Python Code (with or without functions or variables) designed to perform specific 
+# tasks.
+
+# List of Standard Python Modules
+
+import math #for mathematical tasks
+import datetime #for when working with dates and times
+import os # to interact with your operating system
+
+
